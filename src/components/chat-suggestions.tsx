249d086aa7bc7ffb4532db8ee36c91475e5bfa32
Niansuh <139365289+Niansuh@users.noreply.github.com>
2023-12-09 08:39:15 -0500

    Update chat-suggestions.tsx

--- src/components/chat-suggestions.tsx
+++ src/components/chat-suggestions.tsx
@@ -7,7 +7,7 @@ import { BingReturnType } from '@/lib/hooks/use-bing'
 import { SVG } from './ui/svg'
 
 type Suggestions = SuggestedResponse[]
-const helpSuggestions = ['为什么不回应某些主题', '告诉我更多关于必应的资迅', '必应如何使用 AI?'].map((text) => ({ text }))
+const helpSuggestions = ['Why not respond to some topics', 'Tell me more about Bing', 'How does Bing use AI?'].map((text) => ({ text }))
 const suggestionsAtom = atom<Suggestions>([])
 
 type ChatSuggestionsProps = React.ComponentProps<'div'> & Pick<BingReturnType, 'setInput'> & { suggestions?: Suggestions }
