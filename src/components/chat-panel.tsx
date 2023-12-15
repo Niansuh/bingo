90f89c8a4d0103a11747335a3d42d70f39089ca0
Niansuh <139365289+Niansuh@users.noreply.github.com>
2023-12-15 09:50:14 -0800

    Update chat-panel.tsx

--- src/components/chat-panel.tsx
+++ src/components/chat-panel.tsx
@@ -46,89 +46,59 @@ export function ChatPanel({
   resetConversation,
   attachmentList,
   uploadImage,
-  setAttachmentList
+  setAttachmentList,
 }: ChatPanelProps) {
-  const inputRef = React.useRef<HTMLTextAreaElement>(null)
-
-  const [focused, setFocused] = React.useState(false)
-  const [tid, setTid] = React.useState<any>()
-  const voiceListening = useAtomValue(voiceListenAtom)
+  const inputRef = React.useRef<HTMLTextAreaElement>(null);
+  const voiceListening = useAtomValue(voiceListenAtom);
 
   const onSend = useCallback(async () => {
     setTimeout(() => {
       window.scrollTo({
         top: document.body.offsetHeight,
-        behavior: 'smooth'
-      })
-    }, 200)
+        behavior: 'smooth',
+      });
+    }, 200);
 
     if (generating) {
       return;
     }
-    const input = inputRef.current?.value || ''
+    const input = inputRef.current?.value || '';
     if (!input?.trim()) {
-      return
+      return;
     }
-    setInput('')
-    await sendMessage(input)
-  }, [generating, input, sendMessage, setInput])
+    setInput('');
+    await sendMessage(input);
+  }, [generating, input, sendMessage, setInput]);
 
-  const onSubmit = useCallback(async (event: KeyboardEvent<HTMLTextAreaElement>) => {
-    debug('event key', event.key)
-    if (
-      event.shiftKey ||
-      event.ctrlKey ||
-      event.nativeEvent.isComposing ||
-      event.key !== 'Enter'
-    ) {
-      return
-    }
-    event.preventDefault()
+  const onSubmit = useCallback(
+    async (event: KeyboardEvent<HTMLTextAreaElement>) => {
+      if (
+        event.shiftKey ||
+        event.ctrlKey ||
+        event.nativeEvent.isComposing ||
+        event.key !== 'Enter'
+      ) {
+        return;
+      }
+      event.preventDefault();
 
-    onSend()
-  }, [onSend, generating, attachmentList])
-
-  const setBlur = useCallback(() => {
-    clearTimeout(tid)
-    const _tid = setTimeout(() => setFocused(false), 2000);
-    setTid(_tid)
-  }, [tid])
-
-  const setFocus = useCallback(() => {
-    setFocused(true)
-    clearTimeout(tid)
-    inputRef.current?.focus()
-  }, [tid])
+      onSend();
+    },
+    [onSend, generating, attachmentList]
+  );
 
   useEffect(() => {
     if (input) {
-      setFocus()
+      inputRef.current?.focus();
     }
-
-  }, [input, setFocus])
+  }, [input]);
 
   return (
     <div className={cn('chat-panel relative', className)}>
       <div className="action-bar pb-4">
         <div className="action-root" speech-state="hidden" visual-search="" drop-target="">
-          <div className="fade bottom">
-            <div className="background"></div>
-          </div>
-          <div className={cn('outside-left-container', { collapsed: focused })}>
-            <div className="button-compose-wrapper">
-              <button className="body-2 button-compose" type="button" aria-label="New topic" onClick={resetConversation}>
-                <div className="button-compose-content">
-                  <SVG className="pl-2" alt="new topic" src={NewTopic} width={1} fill="currentColor" />
-                  <div className="button-compose-text">New topic</div>
-                </div>
-              </button>
-            </div>
-          </div>
-          <div
-            className={cn('main-container')}
-            onClick={setFocus}
-            onBlur={setBlur}
-          >
+          {/* ... (other elements) */}
+          <div className={cn('main-container')}>
             {input.startsWith('/') && (
               <ChatPrompts onChange={setInput} filter={input.slice(1)} />
             )}
@@ -140,14 +110,28 @@ export function ChatPanel({
                 onKeyDown={onSubmit}
                 rows={1}
                 value={input}
-                onChange={e => setInput(e.target.value.slice(0, 8000))}
-                placeholder={voiceListening ? 'The conversation is ongoing... Just say "Send" when the conversation is complete' : 'Shift + Enter to change the line, enter/select the prompt word'}
+                onChange={(e) => setInput(e.target.value.slice(0, 8000))}
+                placeholder={
+                  voiceListening
+                    ? 'The conversation is ongoing... Just say "Send" when the conversation is complete'
+                    : 'Shift + Enter to change the line, enter/select the prompt word'
+                }
                 spellCheck={false}
                 className="message-input min-h-[24px] w-full text-base resize-none bg-transparent focus-within:outline-none"
               />
-              <Voice className="action-button" setInput={setInput} sendMessage={sendMessage} isSpeaking={isSpeaking} input={input} />
+              <Voice
+                className="action-button"
+                setInput={setInput}
+                sendMessage={sendMessage}
+                isSpeaking={isSpeaking}
+                input={input}
+              />
             </div>
-            <ChatAttachments attachmentList={attachmentList} setAttachmentList={setAttachmentList} uploadImage={uploadImage} />
+            <ChatAttachments
+              attachmentList={attachmentList}
+              setAttachmentList={setAttachmentList}
+              uploadImage={uploadImage}
+            />
             <div className="body-1 bottom-bar">
               <div className="action-button">
                 <ChatImage uploadImage={uploadImage}>
@@ -155,9 +139,16 @@ export function ChatPanel({
                 </ChatImage>
               </div>
               <div className="flex gap-2 items-center">
-                <div className="letter-counter"><span>{input.length}</span>/8000</div>
+                <div className="letter-counter">
+                  <span>{input.length}</span>/8000
+                </div>
                 <button type="submit" className="action-button" onClick={onSend}>
-                  <SVG alt="send" src={input.length ? SendFillIcon : SendIcon} width={18} height={20} />
+                  <SVG
+                    alt="send"
+                    src={input.length ? SendFillIcon : SendIcon}
+                    width={18}
+                    height={20}
+                  />
                 </button>
               </div>
             </div>
@@ -165,5 +156,5 @@ export function ChatPanel({
         </div>
       </div>
     </div>
-  )
+  );
 }
