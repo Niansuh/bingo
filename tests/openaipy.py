4f23db8e4aaa35d785a8f7f3986796f532b3827a
Husnain <139365289+Niansuh@users.noreply.github.com>
2024-02-13 05:06:14 -0600

    Update openaipy.py

--- tests/openaipy.py
+++ tests/openaipy.py
@@ -1,7 +1,7 @@
 import openai
 
 openai.api_key = "dummy"
-openai.api_base = "https://copilot.github1s.tk" # You can change this to your own deployed service. The bingo service version needs to be >= 0.9.0
+openai.api_base = "https://niansuh-bingo.hf.space" # You can change this to your own deployed service. The bingo service version needs to be >= 0.9.0
 # create a chat completion
 completion = openai.ChatCompletion.create(model="gpt-4", stream=False, messages=[{"role": "user", "content": "Hello"}])
 print(completion.choices[0].message.content)
