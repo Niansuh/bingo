5da96749d217c1115d143aa8454e8a1a48989889
Husnain <139365289+Niansuh@users.noreply.github.com>
2024-03-21 15:09:22 +0500

    Update openai-stream.py

--- tests/openai-stream.py
+++ tests/openai-stream.py
@@ -1,6 +1,6 @@
 import openai
 openai.api_key = "dummy"
-openai.api_base = "https://niansuh-bingo.hf.space" # This can be changed to a service deployed by yourself. The bingo service version needs to be >= 0.9.0
+openai.api_base = "https://niansuhai-bingo.hf.space" # This can be changed to a service deployed by yourself. The bingo service version needs to be >= 0.9.0
 
 # create a chat completion
 completion = openai.ChatCompletion.create(model="Creative", stream=True, messages=[{"role": "user", "content": "Hello"}])
