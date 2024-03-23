ae994bba88fc8114a4a70879403b8265cb08c1df
Husnain <139365289+Niansuh@users.noreply.github.com>
2024-03-23 22:12:57 +0500

    Update index.tsx

--- src/components/settings/index.tsx
+++ src/components/settings/index.tsx
@@ -66,7 +66,7 @@ export function Settings() {
               </ExternalLink>
               ，and then open it again
               <ExternalLink href="https://www.bing.com/turing/captcha/challenge">Challenge interface</ExternalLink>
-              右键 》检查。打开开发者工具，在网络里面找到 Challenge 接口 》右键复制》复制为 cURL(bash)，粘贴到此处，然后保存。
+              Right click >Inspect. Open the developer tools, find the Challenge interface in the network, right-click and copy, copy it as cURL (bash), paste it here, and save it.
               <div className="h-2" />
               Graphic and text examples：
               <ExternalLink href="https://github.com/weaigc/bingo#如何获取-bing_header">How to get BING_HEADER</ExternalLink>
